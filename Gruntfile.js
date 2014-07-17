8ef58fcb54c99d41360d374db5caea68713507ad
Oliver Lowe <mrtriangle@gmail.com>
2014-07-17 08:56:19 +0100

    Update grunt file to watch new files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,108 +1,117 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  //Initializing the configuration object
+    //Initializing the configuration object
     grunt.initConfig({
 
-    // Task configuration
-    copy: {
-      main: {
-        expand: true,
-        cwd: 'bower_components/bootstrap/fonts/',
-        src: '**',
-        dest: 'public/assets/fonts/',
-        flatten: true,
-        filter: 'isFile',
-      },
-    },
-
-    less: {
-        development: {
-            options: {
-              compress: true,  //minifying the result
-              optimization: 2,
-              sourceMap: true,
-              sourceMapFilename: "./public/assets/css/global.css.map",
-              sourceMapBasepath: "./public/assets/css/"
+        // Task configuration
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'bower_components/bootstrap/fonts/',
+                src: '**',
+                dest: 'public/assets/fonts/',
+                flatten: true,
+                filter: 'isFile',
             },
-            files: {
-              //compiling global.less into global.css
-              "./public/assets/css/global.css":"./app/assets/css/global.less",
+        },
+
+//        jasmine : {
+//            // Your project's source files
+//            src : 'app/assets/js/src/*.js',
+//            options: {
+//                // Your Jasmine spec files
+//                specs : 'app/assets/spec/**/*spec.js',
+//                // Your spec helper files
+//                helpers : 'app/assets/spec/helpers/*.js'
+//            }
+//        },
+
+        less: {
+            development: {
+                options: {
+                    compress: true,  //minifying the result
+                    optimization: 2,
+                    sourceMap: true,
+                    sourceMapFilename: "public/assets/css/global.css.map",
+                    sourceMapBasepath: "public/assets/css/"
+                },
+                files: {
+                    //compiling global.less into global.css
+                    "public/assets/css/global.css": "app/assets/css/global.less",
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';',
+            },
+            js_global: {
+                src: [
+                    'bower_components/jquery/dist/jquery.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
+                    'app/assets/js/vendor/*.js',
+                    'app/assets/js/src/*.js',
+                    'app/assets/js/global.js'
+                ],
+                dest: 'public/assets/js/global.js',
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false  // Use if you want the names of your functions and variables unchanged
+            },
+            global: {
+                files: {
+                    'public/assets/js/global.js': 'public/assets/js/global.js',
+                }
+            }
+        },
+
+        phpunit: {
+            classes: {
+            },
+            options: {
+            }
+        },
+
+        watch: {
+            js_global: {
+                files: [
+                    //watched files
+                    'bower_components/jquery/jquery.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
+                    'app/assets/js/**/*.js'
+                ],
+                tasks: ['concat:js_global'],
+                options: {
+                    livereload: true
+                }
+            },
+            less: {
+                files: ['app/assets/css/**/*.less'],
+                tasks: ['less'],
+                options: {
+                    livereload: true
+                }
             }
         }
-    },
-
-    concat: {
-      options: {
-        separator: ';',
-      },
-      js_global: {
-        src: [
-          './bower_components/jquery/dist/jquery.js',
-          './bower_components/bootstrap/dist/js/bootstrap.js',
-          './app/assets/js/vendor/*.js',
-          './app/assets/js/partials/*.js',
-          './app/assets/js/global.js'
-        ],
-        dest: './public/assets/js/global.js',
-      }
-    },
-
-    uglify: {
-      options: {
-        mangle: false  // Use if you want the names of your functions and variables unchanged
-      },
-      global: {
-        files: {
-          './public/assets/js/global.js': './public/assets/js/global.js',
-        }
-      }
-    },
-
-    phpunit: {
-        classes: {
-        },
-        options: {
-        }
-    },
-
-    watch: {
-        js_global: {
-          files: [
-            //watched files
-            './bower_components/jquery/jquery.js',
-            './bower_components/bootstrap/dist/js/bootstrap.js',
-            './app/assets/js/**/*.js'
-            ],
-          tasks: ['concat:js_global','uglify:global'],     //tasks to run
-          options: {
-            livereload: true                        //reloads the browser
-          }
-        },
-        less: {
-          files: ['./app/assets/css/**/*.less'],  //watched files
-          tasks: ['less'],                          //tasks to run
-          options: {
-            livereload: true                        //reloads the browser
-          }
-        }
-        //,
-        // tests: {
-        //   files: ['app/controllers/*.php','app/models/*.php'],  //the task will run only when you save files in this location
-        //   tasks: ['phpunit']
-        // }
-      }
     });
 
-  // Plugin loading
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-phpunit');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+    // Plugin loading
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-phpunit');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    // grunt.loadNpmTasks('grunt-contrib-jasmine');
+    // grunt.loadNpmTasks('grunt-hogan');
 
-  // Task definition
-  grunt.registerTask('init', ['copy', 'less', 'concat', 'uglify']);
-  grunt.registerTask('default', ['watch']);
+    // Task definition
+    grunt.registerTask('init', ['copy', 'less', 'concat', 'uglify']);
+    grunt.registerTask('build', ['less', 'concat', 'uglify'])
+    grunt.registerTask('default', ['watch']);
 
 };
