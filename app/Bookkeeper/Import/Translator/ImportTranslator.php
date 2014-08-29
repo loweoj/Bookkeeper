04a9933096c543344e6e9f489e57f0b06c1bf01c
Oliver Lowe <mrtriangle@gmail.com>
2014-08-29 17:34:24 +0100

    Validate transformer implementations return expected data

--- app/Bookkeeper/Import/Translator/ImportTranslator.php
+++ app/Bookkeeper/Import/Translator/ImportTranslator.php
@@ -4,6 +4,14 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ImportTranslator {
 
+    /**
+     * @var
+     */
+    private $transformer;
+
+    /**
+     * @var
+     */
     private $extension;
 
     /**
@@ -26,14 +34,44 @@ class ImportTranslator {
      * Return an array of Bank Accounts with transactions
      *
      * @param        $data
-     * @param string $map   CSV Data Map array
+     * @param string $map CSV Data Map array
      * @return array
      */
     public function makeAccountsArray($data, $map = 'null')
     {
-        $transformer = $this->fetchTransformer();
 
-        return $transformer->transform($data, $map);
+        $this->transformer = $this->fetchTransformer();
+
+        $transformedData = $this->transformer->transform($data, $map);
+
+        return $this->validateTransformedData($transformedData);
+    }
+
+    /**
+     * Make sure any transformer implementation
+     * returns the expected data.
+     *
+     * @param $data
+     * @return mixed
+     * @throws \Exception
+     */
+    protected function validateTransformedData($accountData)
+    {
+        foreach( $accountData as $data) {
+            if ( ! isset($data['account-number'])
+                or ! isset($data['sort-code'])
+                or ! isset($data['transactions'])
+                or ! isset($data['transactions'][0]['date'])
+                or ! isset($data['transactions'][0]['payee'])
+                or ! isset($data['transactions'][0]['amount'])
+                or ! isset($data['transactions'][0]['type'])
+                or ! isset($data['transactions'][0]['description'])
+            ) {
+                throw new \Exception(get_class($this->transformer) . '::transform() does not return expected array');
+            }
+        }
+
+        return $accountData;
     }
 
     /**
@@ -51,8 +89,9 @@ class ImportTranslator {
         $namespace = 'Bookkeeper\\Import\\Parser\\';
 
         if ($extension == 'csv') {
-            return \App::make($namespace .'CsvParser');
+            return \App::make($namespace . 'CsvParser');
         }
+
         return \App::make($namespace . 'OfxParser');
     }
 
@@ -64,12 +103,9 @@ class ImportTranslator {
         $namespace = 'Bookkeeper\\Import\\Transformer\\';
 
         if ($this->extension == 'csv') {
-            return \App::make($namespace .'CsvTransformer');
+            return \App::make($namespace . 'CsvTransformer');
         }
+
         return \App::make($namespace . 'OfxTransformer');
-
     }
-
-
-
-} 
+}
